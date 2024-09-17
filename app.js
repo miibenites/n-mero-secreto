3f2ff3b83889f3144adfb107c057fb1407ec01f4
miibenites <127204728+miibenites@users.noreply.github.com>
2024-09-17 16:26:50 -0300

    add

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 alert("Boas-Vindas ao Jogo do Número Secreto");
 let numeroSecreto = 4;
-let chgute = prompt("Escolha um número entre 1 e 10?");
+let chute = prompt("Escolha um número entre 1 e 10?");
+if(chute== numeroSecreto){
+    console.log("Isso aí!!! Você descobriu o número secreto(4)")
+}
