7f34528cfc5169fcc4271757971523a3c3a5a55f
miibenites <127204728+miibenites@users.noreply.github.com>
2024-09-25 11:53:52 -0300

    att

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 alert("Boas-Vindas ao Jogo do Número Secreto");
 let numeroSecreto = 4;
 let chute = prompt("Escolha um número entre 1 e 10?");
+
+
 if(chute== numeroSecreto){
-    console.log("Isso aí!!! Você descobriu o número secreto(4)")
+    console.log(`Isso aí!!! Você descobriu o número secreto ${numeroSecreto}`)
+}
+else{
+    if(numeroSecreto> chute){
+        alert(`O número secreto é maior que ${chute}`)
+    } else{
+        alert(`O número secreto é menor que ${chute}`)
+    }
 }
